28a45a8f2c7866890a9f151ccb5ac9068f05dd4e
Moabi <moabisethojane@gmail.com>
2022-07-20 15:17:07 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React from 'react';
+import { View, Image, StyleSheet,Text } from 'react-native';
+import fggc from './assets/icons/birthdaygirl.jpg'
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    backgroundColor:"#fcaa6f",
+    flex: 1,
+    alignContent:"center",
+    alignItems:"center",
+    fontSize:30,
+    maxWidth:1024,
+  },
+  tinyLogo: {
+    width: 300,
+    height: 300,
+    borderRadius:20,
+  },
+  headerhead:{
+    fontSize:42,
+    color: '#ff3333',
+    fontWeight:'bold',
+  },
+  headercontent:{
+    color: '#ffffcc',
+    fontSize: 60,
+  },
+  girlname:{
+    fontSize: 40,
+    color:'#ff5050',
+  },
+  girlcontent:{
+    color:"#4d0026",
+ 
+    fontSize : 32,
+  }
+});
+
+const DisplayAnImage = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text style={styles.headerhead}>Happy </Text>
+    <Text style={styles.headercontent}>Birthday</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={fggc}
+      />
+      <Text style={styles.girlname}>Mrs Mdange</Text>
+      <Text style={styles.girlcontent}>Wishing you a day filled with happiness<br>
+      </br>
+      and a year filled with joy. Send you<br></br>
+     smiles for every moment of your<br></br>
+     special day...Have a wonderful time<br></br>
+    and a very happy birthday
+    </Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default DisplayAnImage;    
